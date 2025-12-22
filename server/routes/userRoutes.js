d13981df3b386b8003e05e87f861656496be903d
Aman Lodha <aman.22mic7062@vitapstudent.ac.in>
2025-12-22 13:11:28 +0530

    Added Profile Picture feature

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 // @route   GET /api/users
 // @desc    Get all users (for the Network page)
